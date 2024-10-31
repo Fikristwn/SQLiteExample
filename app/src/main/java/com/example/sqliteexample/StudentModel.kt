f85257990ef6ada1696985a388352b178ef07350
WIN 10 <setiawanmfikri@gmail.com>
2024-11-01 01:50:53 +0700

    Rycycler SQLITE example

--- app/src/main/java/com/example/sqliteexample/StudentModel.kt
+++ app/src/main/java/com/example/sqliteexample/StudentModel.kt
@@ -1,7 +1,10 @@
 package com.example.sqliteexample
 
-class StudentModel (
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
+data class StudentModel (
     val nim: String,
     val name: String,
     val age: String
-)
+): Parcelable
